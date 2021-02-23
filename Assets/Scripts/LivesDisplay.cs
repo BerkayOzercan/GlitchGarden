09bf052055ddc6b184341b0e5a2bd17de18fa651
Berkay <berkayster@gmail.com>
2021-02-23 09:12:02 +0300

    First Upload

---
+++ Assets/Scripts/LivesDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LivesDisplay : MonoBehaviour
+{
+    [SerializeField] float baseLives = 3;
+    [SerializeField] int damage = 1;
+    float lives;
+    TMP_Text livesText;
+
+    void Start()
+    {
+        lives = baseLives - PlayerPrefsController.GetDifficulty();
+        livesText = GetComponent<TMP_Text>();
+        UpdateDisplay();
+        Debug.Log("difficulty settings currently is " + PlayerPrefsController.GetDifficulty());
+    }
+
+    private void UpdateDisplay()
+    {
+        livesText.text = lives.ToString();
+    }
+
+    public void TakeLife()
+    {
+        lives -= damage;
+        UpdateDisplay();
+
+        if (lives <= 0)
+        {
+            FindObjectOfType<LevelController>().HandleLoseCondition();
+        }
+    }
+}
